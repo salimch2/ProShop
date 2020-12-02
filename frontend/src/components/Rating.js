2c772ba82d2662bcfcec811c618e8b892a33301c
Chemlal Salim <salimch2.sc@gmail.com>
2020-12-02 13:26:19 +0100

    added mongoose for the M in mern

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import PropTypes from 'prop-types'
+
 const Rating = ({ value, text, color }) => {
   return (
     <div className='rating'>
@@ -69,11 +69,6 @@ const Rating = ({ value, text, color }) => {
 }
 Rating.defaultProps = {
   color: '#f8e825',
-}
-Rating.propTypes = {
-  value: PropTypes.number.isRequired,
-  text: PropTypes.string.isRequired,
-  color: PropTypes.string,
 }
 
 export default Rating
